da169681ed9aeff74ee3f0c1e68ea248a02dda81
Juanjo-M <88793489+Juanjo-M@users.noreply.github.com>
2022-04-27 15:11:40 -0500

    Se añade Favicon Y el  Codigo que hizo el profe para listar

--- app1/FuncSprint1/listarInstructor.py
+++ app1/FuncSprint1/listarInstructor.py
@@ -3,13 +3,25 @@ from django.shortcuts import render
 from app1.models import Instructores
 from django.http.response import HttpResponse
 
+
 def listaInstructores(request):
     template = "listarInstructor.html"
-    nombre = request.GET["Nombre"] 
-    if nombre:
-        instructores = Instructores.objects.filter(Nombre = nombre)   
-        return render(request, template, {'instructores':instructores}) 
-    else:
-        return HttpResponse('error')
+    listaFormulario = ["Nombre","Apellido","NumeroDocumento"]
+    diccionarioValores = dict()
+    for e in listaFormulario:
+        if not request.GET[e]:
+            pass
+        else:
+            diccionarioValores[e] = request.GET[e]
+    instructores = Instructores.objects.filter(Nombre = diccionarioValores["Nombre"])
+    return render(request, template, {'instructores': instructores,
+    'diccionario': diccionarioValores})
+
+    #  nombre = request.GET["Nombre"] 
+    #  if nombre:
+    #      instructores = Instructores.objects.filter(Nombre = nombre)   
+    #      return render(request, template, {'instructores':instructores}) 
+    #  else:
+    #      return HttpResponse('error')
 
 
