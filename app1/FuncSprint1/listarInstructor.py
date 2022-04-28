5c02b23fefac0bb26531223093f02afc1c33c466
ngdiaz02 <ngdiaz02@misena.edu.co>
2022-04-27 23:26:19 -0500

    Mejora al CRUD

--- app1/FuncSprint1/listarInstructor.py
+++ app1/FuncSprint1/listarInstructor.py
@@ -1,27 +1,34 @@
+import re
 from django.http import request
 from django.shortcuts import render
-from app1.models import Instructores
+from app1.models import Instructores, Contratacion
 from django.http.response import HttpResponse
 
 
 def listaInstructores(request):
     template = "listarInstructor.html"
-    listaFormulario = ["Nombre","Apellido","NumeroDocumento"]
-    diccionarioValores = dict()
-    for e in listaFormulario:
-        if not request.GET[e]:
-            pass
-        else:
-            diccionarioValores[e] = request.GET[e]
-    instructores = Instructores.objects.filter(Nombre = diccionarioValores["Nombre"])
-    return render(request, template, {'instructores': instructores,
-    'diccionario': diccionarioValores})
-
-    #  nombre = request.GET["Nombre"] 
-    #  if nombre:
-    #      instructores = Instructores.objects.filter(Nombre = nombre)   
-    #      return render(request, template, {'instructores':instructores}) 
-    #  else:
-    #      return HttpResponse('error')
+    if c:=request.GET["Nombre"]:
+        qNombre = Instructores.objects.filter(Nombre = c)
+    else:
+        qNombre = Instructores.objects.all()
+    if c:=request.GET["Apellido"]:
+        qApellido = qNombre.filter(Apellido = c)
+    else:
+        qApellido = qNombre.all()
+    if c:=request.GET["NumeroDocumento"]:
+        qNumeroDocumento = qApellido.filter(NumeroDocumento = c)
+    else:
+        qNumeroDocumento = qApellido.all()
+    if c:=request.GET["TipoDocumento"]:
+        qTipoDocumento = qNumeroDocumento.filter(TipoDocumento = c)
+    else:
+        qTipoDocumento = qNumeroDocumento.all()
+    if c:=request.GET["numeroCelular"]:
+        qnumeroCelular = qTipoDocumento.filter(numeroCelular = c)
+    else:
+        qnumeroCelular = qTipoDocumento.all()
+    
+       
+    return render(request, template, {'instructores': qnumeroCelular})
 
 
