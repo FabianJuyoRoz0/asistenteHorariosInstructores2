b3f14b734552385416b9a9eace77dd0de28c2d74
FabianJuyoRoz0 <fajuyo6@misena.edu.co>
2022-10-11 13:19:57 -0500

    Cambios cargarbd y obtenerhoras

--- app1/FuncSprint1/HorasInstructor.py
+++ app1/FuncSprint1/HorasInstructor.py
@@ -1,11 +1,7 @@
 from django.http import request
 from django.shortcuts import render
-<<<<<<< HEAD
 from app1.models import Instructores
 
-=======
-from app1.models import Contratacion,Instructores
->>>>>>> 8152977f0a761e9c3ec2c8cbb16a4849b89c8233
 
 def obtenerHoras(request):
     template = "horasInstructor.html"
@@ -17,11 +13,7 @@ def obtenerHoras(request):
             print(type(documentoIngresado))
             if Instructores.objects.filter(NumeroDocumento=documentoIngresado).exists():
                 id= Instructores.objects.filter(NumeroDocumento=documentoIngresado).values('id')[0]['id']
-<<<<<<< HEAD
                 horas=Instructores.objects.filter(id=id).values('horasMensualFormacion')[0]['horasMensualFormacion']
-=======
-                horas=Contratacion.objects.filter(id_Instructor_id=id).values('horasMensualFormacion')[0]['horasMensualFormacion']
->>>>>>> 8152977f0a761e9c3ec2c8cbb16a4849b89c8233
                 nombre=Instructores.objects.filter(id=id).values('Nombre')[0]['Nombre']
                 apellido=Instructores.objects.filter(id=id).values('Apellido')[0]['Apellido']
                 print(nombre)
--- app1/FuncSprint1/instructor.py
+++ app1/FuncSprint1/instructor.py
@@ -1,8 +1,5 @@
 from pickle import GET
-<<<<<<< HEAD
 from django.db import IntegrityError
-=======
->>>>>>> 8152977f0a761e9c3ec2c8cbb16a4849b89c8233
 from django.http import request
 from django.shortcuts import get_object_or_404, render
 from app1.models import Instructores
@@ -13,32 +10,21 @@ from datetime import date, datetime
 
 def cargarBDinicial(request):
     template = "CargarBD.html"
-<<<<<<< HEAD
     aviso=[]
     prompt = 'Ingrese el archivo .csv'
     prompt2= 'Instructores repetidos: '
-=======
-    prompt = 'Ingrese el archivo .csv'
->>>>>>> 8152977f0a761e9c3ec2c8cbb16a4849b89c8233
     render(request, template, {'prompt':prompt}) 
     #Validacion
     if request.method=="POST":
         prompt="El archivo no era de extension .csv o tenia diferente estructura"
-<<<<<<< HEAD
         prompt2="Instructores repetidos: "
-=======
->>>>>>> 8152977f0a761e9c3ec2c8cbb16a4849b89c8233
         csv_file = request.FILES['file']
         if csv_file.name.endswith('.csv'):
             data_set = csv_file.read().decode('UTF-8')
             io_string = io.StringIO(data_set)
             for row in csv.reader(io_string, delimiter=',', quotechar="|"):
-<<<<<<< HEAD
                 if 'Nombre' in row[0] and 'Apellido' in row[1] and 'Tipo de documento' in row[2] and 'Numero de identificacion' in row[3] and 'Fecha Inicio' in row[4] and 'Fecha Fin' in row[5] and 'Supervisor' in row[6] and 'Horas mensuales' in row[7] and 'Correo electrónico' in row[8] and 'Número celular' in row[9] and 'Tipo Contrato' in row[10]:
                     prompt="El archivo csv se cargo exitosamente" 
-=======
-                if 'Nombre' in row[0] and 'Apellido' in row[1] and 'Tipo de documento' in row[2] and 'Numero de identificacion' in row[3] and 'Fecha Inicio' in row[4] and 'Fecha Fin' in row[5] and 'Supervisor' in row[6] and 'Horas mensuales' in row[7] and 'Correo electrónico' in row[8] and 'Número celular' in row[9]:
->>>>>>> 8152977f0a761e9c3ec2c8cbb16a4849b89c8233
                     for column in csv.reader(io_string, delimiter=',', quotechar="|"):
                         try:
                             registro1=Instructores(
@@ -48,7 +34,6 @@ def cargarBDinicial(request):
                                 NumeroDocumento = column[3],
                                 correoElectronico = column[8],
                                 numeroCelular = column[9],
-<<<<<<< HEAD
                                 Fecha_Inicio = datetime.strptime(column[4], '%Y-%m-%d'),
                                 Fecha_Fin = datetime.strptime(column[5], '%Y-%m-%d'),
                                 supervisora = column[6],
@@ -96,58 +81,3 @@ def listaInstructores(request):
 
 #     return render(request,template)
     
-=======
-                                )
-                            registro1.save()
-                            
-                            
-                            Contratacion.objects.create(
-                            Fecha_Inicio = datetime.strptime(column[4], '%Y-%m-%d'),
-                            Fecha_Fin = datetime.strptime(column[5], '%Y-%m-%d'),
-                            supervisora = column[6],
-                            id_Instructor = Instructores.objects.filter(NumeroDocumento = column[3])[0],
-                            horasMensualFormacion = column[7]
-                                 )
-                        except:
-                            continue
-                                            
-                    prompt="El archivo csv se cargo exitosamente"               
-                    return render(request, template, {'prompt':prompt})
-
-
-   
-
-
-    return render(request, template, {"prompt":prompt})
-
-def editarInstructor(request):
-    pass
-    
-
-# def listaInstructores(request):
-#     template = "listarInstructor.html"
-#     if c:=request.GET["Nombre"]:
-#         qNombre = Instructores.objects.filter(Nombre = c)
-#     else:
-#         qNombre = Instructores.objects.all()
-#     if c:=request.GET["Apellido"]:
-#         qApellido = qNombre.filter(Apellido = c)
-#     else:
-#         qApellido = qNombre.all()
-#     if c:=request.GET["NumeroDocumento"]:
-#         qNumeroDocumento = qApellido.filter(NumeroDocumento = c)
-#     else:
-#         qNumeroDocumento = qApellido.all()
-#     if c:=request.GET["TipoDocumento"]:
-#         qTipoDocumento = qNumeroDocumento.filter(TipoDocumento = c)
-#     else:
-#         qTipoDocumento = qNumeroDocumento.all()
-#     if c:=request.GET["numeroCelular"]:
-#         qnumeroCelular = qTipoDocumento.filter(numeroCelular = c)
-#     else:
-#         qnumeroCelular = qTipoDocumento.all()
-    
-       
-#     return render(request, template, {'instructores': qnumeroCelular})
-
->>>>>>> 8152977f0a761e9c3ec2c8cbb16a4849b89c8233
